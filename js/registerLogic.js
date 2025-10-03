a58cf70776677ff4f4fcbeb3b6eba0e5f10f513b
rajbirmajhi07 <mrajbir96@gmail.com>
2025-10-04 01:31:10 +0530

    Form started

--- js/registerLogic.js
+++ js/registerLogic.js
@@ -54,8 +54,8 @@ document.addEventListener('DOMContentLoaded', () => {
       return;
     }
 
-    if (password.length < 6) {
-      alert('Password must be at least 6 characters long.');
+    if (password.length < 8) {
+      alert('Password must be at least 8 characters long.');
       return;
     }
 
